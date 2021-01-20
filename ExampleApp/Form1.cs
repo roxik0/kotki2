d7d1ab97aec0d2cb4b6f9e196d75fc08669f8f63
Arek Koralewski <example@example.com>
2021-01-20 19:37:15 +0100

    button with message.

--- ExampleApp/Form1.cs
+++ ExampleApp/Form1.cs
@@ -16,5 +16,9 @@ namespace ExampleApp
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Git jest git");            
+        }
     }
 }
